2504ac99fa8e09719f75588bbfaa09b9b473339b
ghost-83 <ghost_83@mail.ru>
2019-12-24 18:22:16 +0700

    add library JSON

--- src/main/java/ru/ghost/keyri/ControllerSetting.java
+++ src/main/java/ru/ghost/keyri/ControllerSetting.java
@@ -1,11 +1,18 @@
 package ru.ghost.keyri;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.channels.FileChannel;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class ControllerSetting {
 
@@ -32,6 +39,24 @@ public class ControllerSetting {
 
     @FXML
     void initialize() {
+        btn_file.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                Stage openFile = new Stage();
+                FileChooser fileChooset = new FileChooser();
+                File file = fileChooset.showOpenDialog(openFile);
+                leb_file.setText(file.getAbsolutePath());
+            }
+        });
+
+        btn_save.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                SaveSetting saveSetting = new SaveSetting();
+                saveSetting.setJsonFileName(leb_file.getText());
+                saveSetting.setPortCOM(txt_port.getText());
+                saveSetting.setSpeedBOD(txt_bod.getText());
+            }
+        });
 
     }
 }
+
--- src/main/java/ru/ghost/keyri/Setting.java
+++ src/main/java/ru/ghost/keyri/Setting.java
@@ -2,11 +2,38 @@ package ru.ghost.keyri;
 
 public class Setting {
     private String jsonFileName;
-    private String confFileName;
     private int portCOM;
     private int speedBOD;
 
-    public void OpenWindowSetting(){
+    public String getJsonFileName() {
+        return jsonFileName;
+    }
+
+    public void setJsonFileName(String jsonFileName) {
+        this.jsonFileName = jsonFileName;
+    }
+
+    public int getPortCOM() {
+        return portCOM;
+    }
+
+    public void setPortCOM(int portCOM) {
+        this.portCOM = portCOM;
+    }
+
+    public int getSpeedBOD() {
+        return speedBOD;
+    }
+
+    public void setSpeedBOD(int speedBOD) {
+        this.speedBOD = speedBOD;
+    }
+
+    public void saweSetting(){
+
+    }
+
+    public void openConfig(){
 
     }
 }
